fdbdf2087825b56198f0fc04cdc5ca047f847bbc
Julianne Keijzer <91735630+JulianneKeijzer@users.noreply.github.com>
2022-03-03 13:51:39 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("Hello Winc Academy students")
+// this is a large sum
+console.log(1230941 * 1823794);
+console.log(637263 / 54);
+
+/* console.log(1230941 * 1823794);
+console.log(637263 / 54); */
+// Code that is in a comment will not be seen in the console.
